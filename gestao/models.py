f082de2297ed6c42af1dae0c4d3fa3f600612a43
flavioborgesnunes <innovatoreegenharia@gmal.com>
2023-08-21 14:18:16 -0300

    estilizando dashboard inicio

--- gestao/models.py
+++ gestao/models.py
@@ -12,10 +12,18 @@ class Qualidade(models.Model):
         ('PPT', 'ppt')
 
     ]
+    OPC_SUB =[
+        ('finaceiro', '/financeiro/'),
+        ('vendas', '/vendas/')
+    ]
     nome = models.CharField(max_length=50, null=False, blank=False)
-    doc = models.FileField( upload_to="docs/qualidade/%Y/%m/", blank=True)
     modificado = models.DateTimeField(default=datetime.now, blank=False)
     tipo = models.CharField(max_length=150,choices= OPC_DOC, default='')
+    subpasta = models.CharField(max_length=100, choices=OPC_SUB ,blank=False, default='')
+
+    def get_doc_subpasta(instance, filename):
+        return f"docs/qualidade/{instance.subpasta}/{filename}"
+    doc = models.FileField( upload_to=get_doc_subpasta, blank=True)
 
     def __str__(self):
         return self.nome
