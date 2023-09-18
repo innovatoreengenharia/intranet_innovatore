fe9b429b486bb347f9399cb20fc5efb221e69ff0
flavioborgesnunes <innovatoreegenharia@gmal.com>
2023-09-18 16:37:34 -0300

    Dasboard até div do tempo

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from usuario.models import Perfil
+from django.utils import timezone
 
 def home(request):
     if not request.user.is_authenticated:
         return redirect('login')
+    
+    mes_atual = timezone.now().month
+    aniversariantes = Perfil.objects.filter(nascimento__month=mes_atual)
     id_usuario = request.user.id
     perfil = Perfil.objects.filter(usuario_id = id_usuario).first()
 
@@ -13,6 +17,7 @@ def home(request):
 
     context ={
         'perfil':perfil,
+        'aniversariantes':aniversariantes,
     }
 
     return render(request, 'dashboard/home.html', context)
