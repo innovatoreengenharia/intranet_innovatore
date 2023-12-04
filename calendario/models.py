5a4b20a3e793c7b3fa6e67204154b252ffa308ac
flavioborgesnunes <innovatoreegenharia@gmal.com>
2023-12-04 14:16:35 -0300

    calendario - Versão final

--- calendario/models.py
+++ calendario/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Events(models.Model):
+    class Meta:
+        verbose_name_plural = "Eventos"
+
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, null=True, blank=True)
     start = models.DateTimeField(null=True, blank=True)
     end = models.DateTimeField(null=True, blank=True)
     public = models.BooleanField(default=False, null=True, blank=True)
+    allDay = models.BooleanField(default=False, null=True, blank=True)
--- calendario/views.py
+++ calendario/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from usuario.models import Perfil
 from django.http import JsonResponse
 from calendario.models import Events
+from datetime import datetime, date, timedelta
+
+TIME_ZONE = "UTC"
 
 
 def calendario(request):
@@ -24,18 +27,46 @@ def calendario(request):
 def all_events(request):
     id_usuario = request.user.id
     all_events = Events.objects.filter(user=id_usuario)
+    aniversarios = Perfil.objects.all()
+    publicos = Events.objects.filter(public=True)
+    data_atual = date.today()
+    ano_atual = data_atual.year
 
     out = []
+
+    for publico in publicos:
+        out.append(
+            {
+                "title": publico.name,
+                "start": publico.start.strftime("%Y-%m-%d %H:%M"),
+                "end": publico.end.strftime("%Y-%m-%d %H:%M"),
+                "allDay": publico.allDay,
+            }
+        )
+
+    for aniversario in aniversarios:
+        out.append(
+            {
+                "title": f"Aniversariante {aniversario.nome} {aniversario.sobrenome}",
+                "start": f"{ano_atual}-{aniversario.nascimento.month:02d}-{aniversario.nascimento.day:02d}",
+                "allDay": True,
+            }
+        )
+
     for event in all_events:
+        start = event.start - timedelta(hours=3)
+        end = event.end - timedelta(hours=3)
         out.append(
             {
                 "title": event.name,
                 "user": event.user_id,
                 "id": event.id,
-                "start": event.start.strftime("%Y-%m-%d"),
-                "end": event.end.strftime("%Y-%m-%d"),
+                "start": start.strftime("%Y-%m-%d %H:%M"),
+                "end": end.strftime("%Y-%m-%d %H:%M"),
+                "allDay": event.allDay,
             }
         )
+
     return JsonResponse(out, safe=False)
 
 
@@ -44,9 +75,39 @@ def add_event(request):
     end = request.GET.get("end", None)
     title = request.GET.get("title", None)
     user = request.GET.get("user", None)
-    event = Events(name=str(title), start=start, end=end, user_id=int(user))
+
+    try:
+        if "T" in start and "Z" in start:
+            start = datetime.strptime(start, "%Y-%m-%dT%H:%M:%SZ")
+        else:
+            start = datetime.strptime(start, "%Y-%m-%d")
+
+        if "T" in end and "Z" in end:
+            end = datetime.strptime(end, "%Y-%m-%dT%H:%M:%SZ")
+        else:
+            end = datetime.strptime(end, "%Y-%m-%d")
+    except ValueError:
+        # Tratamento de exceção se as strings de data não estiverem no formato esperado
+        data = {"error": "Formato de data inválido."}
+        return JsonResponse(data, status=400)
+
+    # Verificando se a diferença entre as datas é maior que 12 horas
+    if (end - start) > timedelta(hours=12):
+        allDay = True
+    else:
+        allDay = False
+
+    # Criando e salvando o evento
+    event = Events(
+        name=str(title),
+        start=start,
+        end=end,
+        user_id=int(user),
+        allDay=allDay,
+    )
     event.save()
-    data = {}
+
+    data = {"success": "Evento adicionado com sucesso."}
     return JsonResponse(data)
 
 
