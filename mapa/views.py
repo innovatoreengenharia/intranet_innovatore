be7da164637b4c71c014ca0186b337f3efd934c7
flavioborgesnunes <innovatoreegenharia@gmal.com>
2024-01-30 15:18:51 -0300

    Mapa(obras)

--- mapa/views.py
+++ mapa/views.py
@@ -8,11 +8,33 @@ def mapa(request):
     
     id_usuario = request.user.id
     perfil = Perfil.objects.get(usuario_id=id_usuario)
-    colaboradores_dexco = Perfil.objects.filter(obra_trabalho__icontains="dexco")
-    
+
+    # CEARÁ
+    colaboradores_canadian = Perfil.objects.filter(obra_trabalho__icontains="canadian")
+
+    # MARANHÃO
+    colaboradores_cibra = Perfil.objects.filter(obra_trabalho__icontains="cibra")
+
+    # Pernanbuco
+    colaboradores_nebras = Perfil.objects.filter(obra_trabalho__icontains="nebrás")
+
+
+    colaboradores_ajinomoto = Perfil.objects.filter(obra_trabalho__icontains="ajinomoto")
+    colaboradores_rousselot = Perfil.objects.filter(obra_trabalho__icontains="rousselot")
+    colaboradores_agropalma = Perfil.objects.filter(obra_trabalho__icontains="agropalma")
+    colaboradores_cobb = Perfil.objects.filter(obra_trabalho__icontains="cobb")
+    colaboradores_meapler = Perfil.objects.filter(obra_trabalho__icontains="meapler")
+
     context = {
         "perfil": perfil,
-        "colaboradores_dexco": colaboradores_dexco,
+        "colaboradores_canadian": colaboradores_canadian,
+        "colaboradores_cibra": colaboradores_cibra,
+        "colaboradores_nebras":colaboradores_nebras,
+        "colaboradores_ajinomoto": colaboradores_ajinomoto,
+        "colaboradores_rousselot": colaboradores_rousselot,
+        "colaboradores_agropalma": colaboradores_agropalma,
+        "colaboradores_cobb": colaboradores_cobb,
+        "colaboradores_meapler": colaboradores_meapler,
     }
 
     return render(request, "mapa/mapa.html", context)
