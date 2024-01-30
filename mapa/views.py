a6a27246a4d909cf62c6fe14e2c4e939f3d012c6
flavioborgesnunes <innovatoreegenharia@gmal.com>
2024-01-30 09:41:51 -0300

    mapa das obras(dashboard)

--- mapa/views.py
+++ mapa/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from usuario.models import Perfil
 
 
 def mapa(request):
-    return render(request, "mapa/mapa.html")
+    if not request.user.is_authenticated:
+        return redirect("login")
+    
+    id_usuario = request.user.id
+    perfil = Perfil.objects.get(usuario_id=id_usuario)
+    colaboradores_dexco = Perfil.objects.filter(obra_trabalho__icontains="dexco")
+    
+    context = {
+        "perfil": perfil,
+        "colaboradores_dexco": colaboradores_dexco,
+    }
+
+    return render(request, "mapa/mapa.html", context)
