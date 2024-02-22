23175f546cbffb6706598fc8656fa84d0748000b
flavioborgesnunes <innovatoreegenharia@gmal.com>
2024-02-22 12:52:40 -0300

    function add xlsx

--- cartao_visitas/views.py
+++ cartao_visitas/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render, redirect
 from usuario.models import Perfil
 from .models import Cartao_visitas
 from django.core.paginator import Paginator
+import pandas as pd
+
+def importar_dados_do_excel():
+    caminho_arquivo ="./cartoes.xlsx"
+    # Ler o arquivo Excel
+    dados_excel = pd.read_excel(caminho_arquivo)
+
+    # Iterar sobre as linhas do DataFrame
+    for index, linha in dados_excel.iterrows():
+        # Criar uma instância do modelo Cartao_visitas
+        cartao = Cartao_visitas(
+            nome=linha['nome'],
+            empresa=linha['empresa'],
+            email=linha['email'],
+            telefone_fixo=linha['telefone_fixo'],
+            telefone_celular=linha['telefone_celular'],
+            site=linha['site']
+        )
+        # Salvar a instância no banco de dados
+        cartao.save()
 
 
 def cartao_visitas(request):
