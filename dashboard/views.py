88821d1345909266f55b5c7247acdc7cc9fbee0a
flavioborgesnunes <innovatoreegenharia@gmal.com>
2024-02-29 16:46:56 -0300

    previsão do tempo

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,11 +7,34 @@ from django.db.models import Prefetch
 from social.models import Post, Comentarios, Like
 from social.forms import PostForm, ComentarioForm
 
+# Clima Tempo
+
+import requests
+
+def get_weather_data():
+    api_key = '5184c8f1 '
+    # url = f'https://api.hgbrasil.com/weather?key={api_key}' 
+
+    url = f"https://api.hgbrasil.com/weather?key={api_key}&user_ip=remote"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        cidade =data['results']['city']
+        temp =f"{data['results']['temp']}º"
+        print("TENTATIVA DE CIDADE", cidade, temp)
+        return data, cidade, temp
+    else:
+        print("DEU RUIM!!!!!!!!!!!!!!!!")
+        return None
+
+
 
 def home(request):
     if not request.user.is_authenticated:
         return redirect("login")
-
+        
+    data, cidade, temp = get_weather_data()
     id_usuario = request.user.id
     try:
         perfil = Perfil.objects.get(usuario_id=id_usuario)
@@ -27,7 +50,7 @@ def home(request):
     usuarios = Perfil.objects.all()
     usuarios_list = list(usuarios)
     shuffle(usuarios_list)
-    postagens = Post.objects.order_by("-criado_em").prefetch_related(
+    postagens = Post.objects.order_by("-criado_em")[:10].prefetch_related(
         Prefetch("comentarios_set", queryset=Comentarios.objects.order_by("criado_em")),
         Prefetch("like_set", queryset=Like.objects.all()),
     )
@@ -42,5 +65,7 @@ def home(request):
         "usuarios": usuarios,
         "postagens": postagens,
         "user_likes": user_likes,
+        "cidade": cidade,
+        "temp": temp,
     }
     return render(request, "dashboard/home.html", context)
