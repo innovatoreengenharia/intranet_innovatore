47dd3110fe98c1d43aeaa94af5589d096816166c
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-05 13:08:11 -0500

    feat(gunicorn): ajuste a variavel de bind e espacamento do arquivo

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,9 @@
-bind = "0.0.0.0:8000" 
-module = "intranet_innovatore.wsgi:application" 
+bind = "backend:8001"
+module = "intranet_innovatore.wsgi:application"
 
-workers = 4   # Ajuste com base nos recursos do seu servidor 
-worker_connections = 1000 
-threads = 4 
+workers = 4  # Ajuste com base nos recursos do seu servidor
+worker_connections = 1000
+threads = 4
 
-certfile = "/etc/letsencrypt/live/srv479192.hstgr.cloud/fullchain.pem" 
+certfile = "/etc/letsencrypt/live/srv479192.hstgr.cloud/fullchain.pem"
 keyfile = "/etc/letsencrypt/live/srv479192.hstgr.cloud/privkey.pem"
