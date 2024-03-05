4f4d9babbf20dbea96d05d84eccf73b72fbbf03c
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-05 14:04:52 -0500

    feat(gunicorn): comment files path

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,5 +5,5 @@ workers = 4  # Ajuste com base nos recursos do seu servidor
 worker_connections = 1000
 threads = 4
 
-certfile = "/etc/letsencrypt/live/intranet.innovatore.eng.br/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/intranet.innovatore.eng.br/privkey.pem"
+# certfile = "/etc/letsencrypt/live/intranet.innovatore.eng.br/fullchain.pem"
+# keyfile = "/etc/letsencrypt/live/intranet.innovatore.eng.br/privkey.pem"
