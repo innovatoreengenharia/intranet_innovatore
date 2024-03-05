14cb43f238139b9d32d393ca5b3980dfd88426ea
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-05 13:58:56 -0500

    feat(certificates): tente mudar o endereco do servidor para deixar o dominio correto com https

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,5 +5,5 @@ workers = 4  # Ajuste com base nos recursos do seu servidor
 worker_connections = 1000
 threads = 4
 
-certfile = "/etc/letsencrypt/live/srv479192.hstgr.cloud/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/srv479192.hstgr.cloud/privkey.pem"
+certfile = "/etc/letsencrypt/live/intranet.innovatore.eng.br/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/intranet.innovatore.eng.br/privkey.pem"
