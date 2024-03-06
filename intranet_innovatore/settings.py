e679436865942479506b42f5bc60a23a9a41a0a3
flavioborgesnunes <innovatoreegenharia@gmal.com>
2024-03-06 14:26:43 -0300

    Configurar email

--- intranet_innovatore/settings.py
+++ intranet_innovatore/settings.py
@@ -43,15 +43,16 @@ CSRF_TRUSTED_ORIGINS = [
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "login"
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = BASE_DIR / "emails"
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = BASE_DIR / "emails"
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER =
-# EMAIL_HOST_PASSWORD =
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "Intranet@innovatore.eng.br"
+EMAIL_HOST_PASSWORD = "cxobrwfufxjkxmuy"
+
 
 # Application definition
 
