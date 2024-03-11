52dad2178279cf534c806973a7242294dbf42491
flavioborgesnunes <innovatoreegenharia@gmal.com>
2024-03-11 12:03:03 -0300

    url includir tarefas  no url-name

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -1,13 +1,29 @@
 from django.urls import path
-from .views import tarefas, quadro, adicionar_participante, remover_participante,alterar_titulo_quadro
 
+from .views import (
+    adicionar_participante,
+    alterar_titulo_quadro,
+    quadro,
+    remover_participante,
+    tarefas,
+)
 
 urlpatterns = [
-    path("", tarefas , name="tarefas"),
-    path("quadro/<int:id_quadro>", quadro, name="quadro"),
-    path("alterar_titulo_quadro<int:id_quadro>",alterar_titulo_quadro, name="alterar_titulo_quadro"),
-    path("adicionar_participante/<int:id_quadro>", adicionar_participante, name="adicionar_participante"),
-    path("remover_participante/<int:id_quadro>/<int:id_usuario>", remover_participante, name="remover_participante"),
-
-
+    path("", tarefas, name="tarefas"),
+    path("quadro/<int:id_quadro>", quadro, name="tarefas/quadro"),
+    path(
+        "alterar_titulo_quadro<int:id_quadro>",
+        alterar_titulo_quadro,
+        name="tarefas/alterar_titulo_quadro",
+    ),
+    path(
+        "adicionar_participante/<int:id_quadro>",
+        adicionar_participante,
+        name="tarefas/adicionar_participante",
+    ),
+    path(
+        "remover_participante/<int:id_quadro>/<int:id_usuario>",
+        remover_participante,
+        name="tarefas/remover_participante",
+    ),
 ]
